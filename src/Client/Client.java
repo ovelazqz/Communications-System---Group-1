d5043ed99c027d41e8dddaec929886e181f06140
ovelazqz <ovelazqz@icloud.com>
2025-11-06 22:38:21 -0800

    Added to Client

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,8 +1,18 @@
 package Client;
 
 public class Client {
-
-	Client() {
-		
+	
+	private static int count = 0;
+	private final int sessionID;
+	// more attributes required
+	
+	
+	// constructor not complete
+	public Client() {
+		this.sessionID = ++count;
+		// sessionID should probably be created in the server?
+		// upon successful connection, a sessionID is made and returned.
 	}
+	
+	
 }
