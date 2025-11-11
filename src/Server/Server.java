3d1612f2b1e5ef40d4c415f3df113f3c88886946
AaronC91722 <acasas9089@gmail.com>
2025-11-11 13:19:13 -0800

    i did something in server.java

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,5 +1,38 @@
 package Server;
+// idk if this right
+import java.net.serverSocket;
+import java.util.List;
 
+// idk if this is right 
 public class Server {
+	private ServerSocket serverSocket;
+	private List<ClientHandler> clientHandlers;
+	private UserCollection userCollection;
+	private boolean isRunning;
+	
+	
+	public void start() {
+		// add code 
+	}
+	
+	public void stop() {
+		// add code
+	}
+							// idk if this right 
+	public void handleRequest(Request request, ClinetHandler handler) {
+		//add code 
+	}
+	
+	public void broadcast(Response response) {
+		//add code 
+	}
+	
+	public void registerClint(ClientHandler handler) {
+		//add code
+	}
+	
+	public void removeClient(ClientHandler handler) {
+		//add code
+	}
 
 }
