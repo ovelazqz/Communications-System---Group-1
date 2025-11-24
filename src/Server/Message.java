337c455feb4a414a75eb37518bc76360ad620352
jimmyq03 <jimmyq01@hotmail.com>
2025-11-23 23:52:23 -0800

    Update Message.java

--- src/Server/Message.java
+++ src/Server/Message.java
@@ -3,53 +3,5 @@ package Server;
 import java.io.Serializable;
 
 public class Message implements Serializable{
-    private static int count = 0;
-    private int uID;
-    protected String type;
-    protected String status;
-    protected String text;
 
-    public Message() {
-        this.type = "";
-        this.status = "";
-        this.text = "";
-    }
-
-    public Message(String type, String status, String text) {
-        this.uID = ++count;
-        setType(type);
-        setStatus(status);
-        setText(text);
-
-    }
-
-    public String getId() {
-      return uID;
-      
-    }
-
-    private void setType(String type) {
-        this.type = type;
-    }
-    public void setStatus(String status) {
-        this.status = status;
-    }
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-    public String getText() {
-        return text;
-    }
-
-
-
-
-} 
+}
