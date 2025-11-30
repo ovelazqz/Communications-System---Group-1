c956434296fc3a3b0e6b8d14d44fd3ad775a8782
ovelazqz <ovelazqz@icloud.com>
2025-11-29 20:47:39 -0800

    Made changes

--- src/Common/User.java
+++ src/Common/User.java
@@ -1,75 +1,65 @@
 package Common;
 
-import java.util.*;
+import java.io.Serializable;
 
-public class User {
-	private UserCollection userCollection;
-	private static int count = 0;
-	private final String uniqueID;
-	private String username;
-	private String password;
-	private USER_ROLE role;
-	private List<String >chatAccess;
-	private boolean modified;
-	
-	public User(String username, String password, USER_ROLE role) {
-		this.uniqueID = "user#" + ++count;
-		if (userCollection.isValidUsername(username)) {
-			// Error account already exists
-		} else {
-			this.setUsername(username);
-		}
-		this.setPassword(password); 
-		this.setRole(role); 		// error role check will occur in GUI
-		this.setModified(false);
-	}
-
-	public String getUniqueID() {
-		return uniqueID;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public USER_ROLE getRole() {
-		return role;
-	}
-
-	public void setRole(USER_ROLE role) {
-		this.role = role;
-	}
-
-	public List<String > getChatAccess() {
-		return chatAccess;
-	}
-
-	public void setChatAccess(List<String > chatAccess) {
-		this.chatAccess = chatAccess;
-	}
-
-	public boolean isModified() {
-		return modified;
-	}
-
-	public void setModified(boolean modified) {
-		this.modified = modified;
-	}
-	
-	
-	// if modified must save, method will go below
-	
-	
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private static int userCount = 0;
+    
+    private String uniqueID;
+    private String username;
+    private String password;
+    private USER_ROLE role;
+   
+    
+    
+    public User(String username, String password, USER_ROLE role) {
+        this.uniqueID = "USER_" + (++userCount);
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        
+    }
+    
+    
+    public String getUniqueID() {
+        return uniqueID;
+    }
+    
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    public USER_ROLE getRole() {
+        return role;
+    }
+    
+    
+    public void setRole(USER_ROLE role) {
+        this.role = role;
+    }
+    
+    
+    
+    @Override
+    public String toString() {
+        return uniqueID + "," + username + "," + password + "," + role;
+    }
 }
