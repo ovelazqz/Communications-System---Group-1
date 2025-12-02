64844c10df5b09d359c60eac9a44abb02e865ad6
lgarciamartinez <lgarciamartinez@horizon.csueastbay.edu>
2025-12-01 16:41:18 -0800

    started tests on chatmanager

--- src/Tests/TestingChatManager.java
+++ src/Tests/TestingChatManager.java
@@ -2,13 +2,18 @@ package Tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 class TestingChatManager {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void createPrivateChat_validRecipients_createsAndStoresChat() {
+		ChatManager manager = new ChatManager();
+		List<String> recipients = Arrays.asList("aaron", "brian");
+		
+		PrivateChat 
 	}
 
 }
